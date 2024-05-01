f5390256767a57082387b0bd2327c2cc34931ee9
Pragalbha Patil <44872076+Pragalbha-Patil@users.noreply.github.com>
2024-05-02 02:42:43 +0530

    killing spree update
    - you can kill slimes now by jumping on them

--- scripts/killzone.gd
+++ scripts/killzone.gd
@@ -2,17 +2,26 @@ extends Area2D
 
 @onready var timer = $Timer
 @onready var kill_sound = $kill
+@onready var collision_shape_2d = $CollisionShape2D
+var kill_player = true
 
 func _on_body_entered(body):
-	kill_sound.play()
-	timer.start()
-	Engine.time_scale = 0.5
-	body.move_local_y(-15)
-	body.rotate(-20)
-	body.get_node("CollisionShape2D").queue_free()
-	
-
+	if !kill_player:
+		pass
+	else:
+		print("player killed")
+		kill_sound.play()
+		timer.start()
+		Engine.time_scale = 0.5
+		body.move_local_y(-15)
+		body.rotate(-20)
+		body.get_node("CollisionShape2D").queue_free()
 
 func _on_timer_timeout():
 	Engine.time_scale = 1
 	get_tree().reload_current_scene()
+
+
+func _on_area_2d_body_entered(body):
+	kill_player = false
+	get_parent().emit_signal("tree_exiting") # this signal is handled by the parent (in our case: "slime") to perform actions
--- scripts/slime.gd
+++ scripts/slime.gd
@@ -1,21 +1,31 @@
 extends Node2D
 
-const SPEED = 50
+const SPEED = 55
 
 var direction = 1
 @onready var ray_cast_right = $RayCastRight
 @onready var ray_cast_left = $RayCastLeft
 @onready var animated_sprite = $AnimatedSprite2D
+@onready var timer = $Timer
+@onready var kill_sound = $AudioStreamPlayer2D
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if ray_cast_right.is_colliding():
+	if is_instance_valid(ray_cast_right) and ray_cast_right.is_colliding():
 		animated_sprite.flip_h = true
 		direction = -1
 
-	if ray_cast_left.is_colliding():
+	if is_instance_valid(ray_cast_left) and ray_cast_left.is_colliding():
 		animated_sprite.flip_h = false
 		direction = 1
 
 	position.x += delta * SPEED * direction
-	
+
+
+func _on_tree_exiting():
+	kill_sound.play()
+	animated_sprite.play("killed")
+	timer.start()
+
+func _on_timer_timeout():
+	self.queue_free()
