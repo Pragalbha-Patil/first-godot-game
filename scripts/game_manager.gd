383b961d713ae4b2c8cecf1c200febc375543076
Pragalbha Patil <44872076+Pragalbha-Patil@users.noreply.github.com>
2024-05-02 14:27:09 +0530

    update
    - game exits on completion
    - new win sfx added
    - bg music is stopped on game completion

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -3,13 +3,23 @@ extends Node
 const TOTAL_COINS = 15
 var score = 0
 @onready var score_label = $ScoreLabel
-@onready var win = $win
+@onready var win_sfx = $win
+@onready var timer = $Timer
+@onready var music = get_node("/root/Music")
 
 func add_point():
 	score += 1
 	if score >= TOTAL_COINS:
-		score_label.text = "You completed the game!"
-		win.play()
+		music.queue_free()
+		timer.start()
 		Engine.time_scale = 0.3
+		score_label.text = "You completed the game!"
+		win_sfx.play()
+		
 	else:
 		score_label.text = "You collected " + str(score) + " coins."
+
+
+func _on_timer_timeout():
+	score_label.text = "Game exiting!"
+	get_tree().quit()
